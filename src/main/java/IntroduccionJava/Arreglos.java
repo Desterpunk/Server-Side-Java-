3ae59c4806da9a054ebcb246cbd52763e49aa5ce
Jhon Serna <jhonalfaber@gmail.com>
2021-08-29 12:57:47 -0500

    Uso de Arreglos

---
+++ src/main/java/IntroduccionJava/Arreglos.java
@@ -0,0 +1,12 @@
+package IntroduccionJava;
+
+public class Arreglos {
+    public static void main(String[] args) {
+        int x[] = new int[10];
+        int y[] = {1,3,4};
+        for (int i = 0; i < x.length; i++) {
+            x[i] = i;
+            System.out.println("x = " + x[i]);
+        }
+    }
+}
