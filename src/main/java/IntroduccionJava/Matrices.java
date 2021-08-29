720d909e5547483f17e33d6d6e8a55f5e64e0bfa
Jhon Serna <jhonalfaber@gmail.com>
2021-08-29 13:03:16 -0500

    Uso de Matrices

---
+++ src/main/java/IntroduccionJava/Matrices.java
@@ -0,0 +1,13 @@
+package IntroduccionJava;
+
+public class Matrices {
+    public static void main(String[] args) {
+        int x[][] = new int[3][3];
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x.length; j++) {
+                x[i][j] = j;
+                System.out.println("x = " + x[i][j]);
+            }
+        }
+    }
+}
