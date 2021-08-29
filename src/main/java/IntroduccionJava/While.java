24cf39962a8ea2ecc87a1c37457d81e3176de1e7
Jhon Serna <jhonalfaber@gmail.com>
2021-08-29 12:11:13 -0500

    primer while

---
+++ src/main/java/IntroduccionJava/While.java
@@ -0,0 +1,11 @@
+package IntroduccionJava;
+
+public class While {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x<=10) {
+            System.out.println("x = " + x);
+            x = x + 2;
+        }
+    }
+}
