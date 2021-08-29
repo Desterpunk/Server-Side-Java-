9dd0ea50adb1042f29ab345b3f4ebec2618da9a8
Jhon Serna <jhonalfaber@gmail.com>
2021-08-29 12:20:26 -0500

    break y continue

---
+++ src/main/java/IntroduccionJava/BreakContinue.java
@@ -0,0 +1,13 @@
+package IntroduccionJava;
+
+public class BreakContinue {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+            if (i == 4) {
+                break;
+            }
+        }
+        System.out.println("haz dejado el ciclo");
+    }
+}
