9c5ddbc623ad88fab5811da8a890b0c722c293eb
Jhon Serna <jhonalfaber@gmail.com>
2021-08-29 11:31:57 -0500

    Variables y operaciones 1

--- src/main/java/PrimerPaquete/HolaMundo.java
+++ src/main/java/PrimerPaquete/HolaMundo.java
@@ -3,6 +3,5 @@ package PrimerPaquete;
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-
     }
 }
---
+++ src/main/java/variablesyoperacionesmatematicas/Variables1.java
@@ -0,0 +1,11 @@
+package variablesyoperacionesmatematicas;
+
+public class Variables1 {
+    public static void main(String[] args) {
+        float variable1;
+        variable1 = 5;
+        float resultado;
+        resultado = variable1 / 2;
+        System.out.println("resultado = " + resultado);
+    }
+}
